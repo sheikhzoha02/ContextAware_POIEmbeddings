162255073e55e00ebe43ebec659c41a2bdd21b8b
Zoha Sheikh <100231242+sheikhzoha02@users.noreply.github.com>
2024-02-10 03:21:04 +0100

    Update problematic_rows.py

--- Module/problematic_rows.py
+++ Module/problematic_rows.py
@@ -1,27 +1,20 @@
 import pandas as pd
 
-# Specify the file causing the issue
 edge_weights_file = '/home/iailab41/sheikhz0/POI-Embeddings-Own-Approach/Data/NYC/nyc_category_edge_weight.csv'
 
-# Initialize a counter for problematic rows
 problematic_rows_count = 0
 
 try:
-    # Use chunksize to read the file in chunks
-    chunksize = 10000  # Adjust this based on your file size
+    chunksize = 10000
     for chunk in pd.read_csv(edge_weights_file, chunksize=chunksize, error_bad_lines=False):
-        # Iterate through each chunk and count the problematic rows
         try:
-            # Check for any NaN values in the chunk
             problematic_rows = chunk[chunk.isnull().any(axis=1)]
             if not problematic_rows.empty:
                 problematic_rows_count += len(problematic_rows)
         except pd.errors.ParserError as e:
             print(f"ParserError in chunk: {e}")
-            # Handle the error or log the problematic chunk
 
 except pd.errors.ParserError as e:
     print(f"ParserError: {e}")
-    # Handle the error or log the problematic line number
 
 print(f"Total count of problematic rows: {problematic_rows_count}")
